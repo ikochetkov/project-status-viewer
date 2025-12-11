9721f13280efefb1f082fa74120a97c93233e302
Ihor Kochetkov <ihorkochetkov@Ihors-MacBook-Air.local>
2025-12-11 18:24:21 +0100

    Rename component tag to x-mobit-status-report-viewer

--- src/table-component/index.js
+++ src/table-component/index.js
@@ -250,7 +250,7 @@ const view = (state, {updateState}) => {
 	);
 };
 
-createCustomElement('x-mobit-table-component', {
+createCustomElement('x-mobit-status-report-viewer', {
 	renderer: {type: snabbdom},
 	view,
 	styles,
@@ -335,7 +335,7 @@ createCustomElement('x-mobit-table-component', {
  * Project Status Viewer Component
  * Displays a table of projects with their status, health metrics, and progress
  *
- * @seismicElement x-mobit-table-component
+ * @seismicElement x-mobit-status-report-viewer
  * @summary Table component for viewing project status and health indicators
  * @uib.label Project Status Viewer
  * @uib.icon list
